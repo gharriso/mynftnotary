f35b5be3dbbb82e3b5feabe2da62420321378032
Guy Harrison <guy@southbanksoftware.com>
2023-01-04 17:03:55 +1100

    WIP

---
+++ cloudRuns/storeFile/index.js
@@ -0,0 +1,59 @@
+import express from 'express'
+
+import {
+    create
+} from 'ipfs-http-client'
+
+import bodyParser from 'body-parser'
+
+import simpleNodeLogger from 'simple-node-logger'
+
+import {addFile} from './storeFile.js'
+
+const log = simpleNodeLogger.createSimpleLogger({
+    timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS',
+})
+
+const app = express()
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({
+    extended: false
+})
+
+let authHeader
+let ipfsGateway
+
+if ('authHeader' in process.env) {
+    authHeader = process.env.authHeader
+} else {
+    log.error("no authHeader in process.env")
+    process.exit(1)
+}
+if ('ipfsGateway' in process.env) {
+    ipfsGateway = process.env.ipfsGateway
+} else {
+    log.error("no ipfsGateway in process.env")
+    process.exit(1)
+}
+
+const port = parseInt(process.env.PORT) || 8080;
+
+app.listen(port, () => {
+    log.info(`listening on port ${port}`);
+});
+
+app.post('/storeFile', jsonParser, async (req, res) => {
+    if ('data' in req.body)  {
+    let response= await addFile(ipfsGateway, authHeader, req.body.data)
+    res.status(200).json(response)
+    } else {
+        res.status(404).send('No Data in payload')
+    }
+});
+
+
+
+
+
+
+
--- cloudRuns/storeFile/storeFile.js
+++ cloudRuns/storeFile/storeFile.js
@@ -1,11 +1,27 @@
-const IPFS = require('ipfs-core')
 
-async function main() {
-    const ipfs = await IPFS.create()
+import {
+    create
+} from 'ipfs-http-client'
+
+export async function addFile(ipfsGateWay, authHeader, fileContent) {
+
+    // 1. Create IPFS instant
+    const ipfs = create({
+        url: `${ipfsGateWay}/api/v0`,
+        headers: {
+            authorization: `Basic ${authHeader}`
+        }
+    });
+
     const {
         cid
-    } = await ipfs.add('Hello world')
-    console.log(cid)
+    } = await ipfs.add(fileContent)
+
+    const fileStat = await ipfs.files.stat("/ipfs/" + cid.toString())
+
+    return {
+        cid: cid.toString(),
+        size: fileStat.cumulativeSize
+    };
 }
 
-main()
